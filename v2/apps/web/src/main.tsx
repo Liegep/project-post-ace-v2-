751ab40f36424fabd84be1f98464eaa8eb83909b
Lilika <slmariew@gmail.com>
2026-09-17 15:56:29 +0200

    Mount admin Kanban search

--- v2/apps/web/src/main.tsx
+++ v2/apps/web/src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { App } from "./App";
+import { AdminKanbanSearch } from "./AdminKanbanSearch";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
       <App />
+      <AdminKanbanSearch />
     </HashRouter>
   </React.StrictMode>,
 );
