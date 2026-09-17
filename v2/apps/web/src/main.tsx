01dcb55a104a757b90f300f335e846de8172d439
Lilika <slmariew@gmail.com>
2026-09-17 16:14:09 +0200

    Enable agenda overflow viewer

--- v2/apps/web/src/main.tsx
+++ v2/apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { App } from "./App";
 import { AdminKanbanSearch } from "./AdminKanbanSearch";
+import { AgendaOverflowViewer } from "./AgendaOverflowViewer";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <HashRouter>
       <App />
       <AdminKanbanSearch />
+      <AgendaOverflowViewer />
     </HashRouter>
   </React.StrictMode>,
 );
